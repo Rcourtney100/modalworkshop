4b087bdb2224aac1d6c24fb17815c0b5178ca12f
Ryan Courtney <ryan.courtney1@gmail.com>
2016-11-16 17:40:49 -0500

    Figured out Modal basics

--- modalworkshop.js
+++ modalworkshop.js
@@ -1,27 +1,14 @@
-// Get the modal
-var modal = document.getElementById('myModal');
+// window.onload = function() {
+// 	document.getElementById('myBtn').addEventListener ('click', function () {
+// 		document.getElementById('myModal').style.display = "block"
+// 	})
+// }
+document.getElementsByClassName('close')[0].addEventListener ('click', function(){
+	document.getElementById('myModal').style.display = "none"
+})
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+setTimeout(showModal,3000)
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-	if (click.target == myBtn)
-    	modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-	if (click.target == close)
-    	modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+function showModal(){
+	document.getElementById('myModal').style.display = "block"
 }
